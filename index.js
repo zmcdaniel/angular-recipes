f0a75601c4268995d3534a77e5993e27529a4975
bhague1281 <bhague1281@gmail.com>
2016-03-17 17:21:30 -0700

    added 404 message

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/api/auth', function(req, res) {
   User.findOne({email: req.body.email}, function(err, user) {
     if (err || !user) return res.status(401).send({message: 'User not found'});
     user.authenticated(req.body.password, function(err, result) {
-      if (err || !result) return res.send({message: 'User not authenticated'});
+      if (err || !result) return res.status(401).send({message: 'User not authenticated'});
 
       var token = jwt.sign(user, secret);
       res.send({user: user, token: token});
