dfac572caca3ea50e2c26481cced1d058440316e
zmcdaniel <zmcdaniel@gmail.com>
2016-09-21 15:32:47 -0700

    Angular factory

--- public/app/services.js
+++ public/app/services.js
@@ -2,3 +2,16 @@ angular.module('RecipeServices', ['ngResource'])
 .factory('Recipe', ['$resource', function($resource) {
   return $resource('/api/recipes/:id');
 }]);
+.factory('Auth', ['$window', function($window){
+  return {
+    saveToken: function(token) {
+      $window.localStorage['secretrecipes-token'] = token;
+    },
+    getToken: function() {
+      return $window.localStorage['secretrecipes-token'];
+    },
+    removeToken: function() {
+      $window.localStorage.removeItem('secretrecipes-token');
+    }
+  }
+}]);
